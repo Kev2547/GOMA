f659ba628c487e1ea1428ada9c99ea6a9cea8bde
Kev2547 <kevimulemberi@gmail.com>
2025-07-22 19:41:10 +0200

    kev

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,29 @@
-// vite.config.ts
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 
 export default defineConfig({
   plugins: [react()],
+  base: '/',
+  optimizeDeps: {
+    exclude: ['lucide-react'],
+  },
   server: {
-    port: 5173
-  }
-})
+    proxy: {
+      '/api': {
+        target: 'https://servertamu.onrender.com',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom'],
+        },
+      },
+    },
+  },
+});
